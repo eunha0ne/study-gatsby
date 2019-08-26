bb0607a4af7dd390619eed1c3d0bc97f49c476c6
eunha0ne <eunha0ne@gmail.com>
2019-08-26 22:45:14 +0900

    Composing custom `useStaticQuery` hooks (#8)

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,12 @@
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
-import PropTypes from "prop-types"
+import { useSiteMetadata } from "../hooks/use-site-metadata"
 
-const Header = ({ data }) => (
-  <header>
-    <h1>{data.site.siteMetadata.title}</h1>
-  </header>
-)
-
-export default props => (
-  <StaticQuery
-    query={graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={ data => <Header data={data} {...props} /> }
-  />
-)
-
-Header.propTypes = {
-  data: PropTypes.shape({
-    site: PropTypes.shape({
-      siteMetadata: PropTypes.shape({
-        title: PropTypes.string.isRequired,
-      }).isRequired,
-    }).isRequired,
-  }).isRequired,
+export default () => {
+  const { title, description } = useSiteMetadata();
+  return (
+    <header>
+      <h1>welcome to {title}</h1>
+      <p>{description}</p>
+    </header>
+  );
 }
